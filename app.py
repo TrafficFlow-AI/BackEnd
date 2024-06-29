cfe1880c07e108cac6f5c7dfface401d8e188f74
JordiPompas <jordi.pompas@bravent.net>
2024-06-29 19:52:25 +0200

    Queue integration

--- app.py
+++ app.py
@@ -1,23 +1,59 @@
 from speed_track import SpeedTrack
-from queue_detection import queue_management
+from queue_detection import QueueTrack
 from flask import Flask, render_template_string, Response
 import time
+
 speed_tracker = SpeedTrack()
+queue_manager = QueueTrack()
+
 app = Flask(__name__)
 
 @app.route('/speed_estimation')
 def speed_estimation():
+    """
+    Route decorator for the '/speed_estimation' endpoint.
+    Returns a response with the speed data obtained from the `speed_tracker` object.
+    The response is in the format of 'multipart/x-mixed-replace; boundary=frame'.
+    """
     return Response(speed_tracker.get_speed(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/get_speed_data')
 def get_speed_data():
+    """
+    A route handler function that continuously yields the latest `json_data` from the `speed_tracker` object.
+
+    Returns:
+        A generator that yields the latest `json_data` as a string in the format "data: {json_data}".
+
+    """
     while True:
         time.sleep(1)
         yield f"data: {speed_tracker.json_data}\n\n"
 
 @app.route('/queue_detection')
 def queue_detection():
-    return Response(queue_management(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    """
+    A route handler function for the '/queue_detection' endpoint.
 
+    This function returns a Flask Response object that contains the output of the `queue_management` method
+    of the `queue_manager` object. The output is in the format of 'multipart/x-mixed-replace; boundary=frame'.
+
+    Returns:
+        A Flask Response object containing the output of the `queue_management` method.
+    """
+    return Response(queue_manager.queue_management(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/get_queue_data')
+def get_queue_data():
+    """
+    A route handler function that continuously yields the latest `json_data` from the `speed_tracker` object.
+
+    Returns:
+        A generator that yields the latest `json_data` as a string in the format "data: {json_data}".
+    """
+    while True:
+        time.sleep(1)
+        yield f"data: {queue_manager.counts_display}\n\n"
+        
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)#,host="192.168.111.109")
--- queue_detection.py
+++ queue_detection.py
@@ -1,51 +1,56 @@
 import cv2
 from ultralytics import YOLO, solutions
 import torch
+from src.custom_queue.queue_management import QueueManager
+class QueueTrack:
+    def __init__(self):
+        self.json_data = None
+            
+    def queue_management(self):
+        cuda_available = torch.cuda.is_available()
+        device = "cuda" if cuda_available else "cpu"
+        # Cargar el modelo YOLO
+        model = YOLO("yolov10m.pt", verbose=True)
+        model.to(device)
+        cap = cv2.VideoCapture("data/videotrafico2.mp4")
+        assert cap.isOpened(), "Error leyendo el archivo de video"
+        
+        w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
+        new_w, new_h = w // 2, h // 2  # Nueva resolución a la mitad
+        video_writer = cv2.VideoWriter("queue_management_rescaled.avi", cv2.VideoWriter_fourcc(*"mp4v"), fps, (new_w, new_h))
+        print(f"Original resolution: {w}x{h}, Rescaled resolution: {new_w}x{new_h}")
+        
+        # Definir la región de interés (ROI)
+        queue_region = [(1920, 1080), (1920, 980), (1120, 500), (1000, 500), (1450, 1080)]
 
-def queue_management():
-    cuda_available = torch.cuda.is_available()
-    device = "cuda" if cuda_available else "cpu"
-    # Cargar el modelo YOLO
-    model = YOLO("yolov10m.pt", verbose=True)
-    model.to(device)
-    cap = cv2.VideoCapture("data/videotrafico2.mp4")
-    assert cap.isOpened(), "Error leyendo el archivo de video"
-    
-    w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
-    new_w, new_h = w // 2, h // 2  # Nueva resolución a la mitad
-    video_writer = cv2.VideoWriter("queue_management_rescaled.avi", cv2.VideoWriter_fourcc(*"mp4v"), fps, (new_w, new_h))
-    print(f"Original resolution: {w}x{h}, Rescaled resolution: {new_w}x{new_h}")
-    
-    # Definir la región de interés (ROI)
-    queue_region = [(1920, 1080), (1920, 980), (1120, 500), (1000, 500), (1450, 1080)]
+        queue = QueueManager(
+            classes_names=model.names,
+            reg_pts=queue_region,
+            line_thickness=2,
+            fontsize=0.75,
+            region_color=(255, 144, 31),
+        )
 
-    queue = solutions.QueueManager(
-        classes_names=model.names,
-        reg_pts=queue_region,
-        line_thickness=2,
-        fontsize=0.75,
-        region_color=(255, 144, 31),
-    )
+        car_classes = [2, 3, 5, 7]  # Clases correspondientes a coches
 
-    car_classes = [2, 3, 5, 7]  # Clases correspondientes a coches
+        while cap.isOpened():
+            success, im0 = cap.read()
 
-    while cap.isOpened():
-        success, im0 = cap.read()
+            if success:
+                im0_rescaled = cv2.resize(im0, (new_w, new_h))
+                tracks = model.track(im0_rescaled, show=False, persist=True, verbose=False, classes=car_classes)  # Solo clases de coches
+                out = queue.process_queue(im0_rescaled, tracks)
 
-        if success:
-            im0_rescaled = cv2.resize(im0, (new_w, new_h))
-            tracks = model.track(im0_rescaled, show=False, persist=True, verbose=False, classes=car_classes)  # Solo clases de coches
-            out = queue.process_queue(im0_rescaled, tracks)
+                # Dibujar el contador en la parte superior izquierda
+                # cv2.putText(im0, f'Count: {queue.view_queue_counts}', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 0), 3)
 
-            # Dibujar el contador en la parte superior izquierda
-            # cv2.putText(im0, f'Count: {queue.view_queue_counts}', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 0), 3)
-
-            video_writer.write(im0_rescaled)
-            ret, buffer = cv2.imencode('.jpg', im0_rescaled)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+                video_writer.write(im0_rescaled)
+                ret, buffer = cv2.imencode('.jpg', im0_rescaled)
+                frame = buffer.tobytes()
+                self.counts_display = queue.counts_display
+                yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
-    cap.release()
-    cv2.destroyAllWindows()
+        cap.release()
+        cv2.destroyAllWindows()
