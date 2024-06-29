b03006a757b4a55c00c8ded03d1bd214d2b80435
JordiPompas <jordi.pompas@bravent.net>
2024-06-29 18:39:43 +0200

    Stream Data

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from speed_track import get_speed
+from speed_track import SpeedTrack
 from queue_detection import queue_management
-from flask import Flask, render_template, Response
-
+from flask import Flask, render_template_string, Response
+import time
+speed_tracker = SpeedTrack()
 app = Flask(__name__)
-    
+
 @app.route('/speed_estimation')
 def speed_estimation():
-    return Response(get_speed(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(speed_tracker.get_speed(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/get_speed_data')
+def get_speed_data():
+    while True:
+        time.sleep(1)
+        yield f"data: {speed_tracker.json_data}\n\n"
 
 @app.route('/queue_detection')
 def queue_detection():
