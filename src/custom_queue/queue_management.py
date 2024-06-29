96a88f5a7523b54c490b603b6e59a00264b3dfe9
JordiPompas <jordi.pompas@bravent.net>
2024-06-29 22:52:22 +0200

    Poligon view

--- src/custom_queue/queue_management.py
+++ src/custom_queue/queue_management.py
@@ -142,7 +142,7 @@ class QueueManager:
     def display_frames(self):
         """Displays the current frame with annotations."""
         if self.env_check:
-            #self.annotator.draw_region(reg_pts=self.reg_pts, thickness=self.region_thickness, color=self.region_color)
+            self.annotator.draw_region(reg_pts=self.reg_pts, thickness=self.region_thickness, color=self.region_color)
             #cv2.namedWindow(self.window_name)
             #cv2.imshow(self.window_name, self.im0)
             # Close window on 'q' key press
